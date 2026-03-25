10e212cd5b8fcadcd5759f810139d3370070adde
shalvik29 <shahshalvik29@gmail.com>
2026-03-25 11:24:34 -0400

    change padding top on services [id]

--- app/services/[id]/page.tsx
+++ app/services/[id]/page.tsx
@@ -18,7 +18,7 @@ export default function ServiceDetailPage({ params }: { params: { id: string } }
   }
 
   return (
-    <div className="flex flex-col min-h-screen pt-32 md:pt-44">
+    <div className="flex flex-col min-h-screen pt-32 md:pt-40">
       {/* Hero Section */}
       <section className="relative h-[400px] md:h-[500px] flex items-center justify-center">
         <Image
